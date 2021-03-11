c81568f45ee981c0d5ce5fc51a1d4de4c21b1272
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-03-11 19:14:29 +0200

    added axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,12 +9,13 @@
 
 import Todos from "../components/Todos"
 import AddTodo from "../components/AddTodo"
+import axios from "axios"
 export default {
   name: 'Home',
   components: {
     Todos,
     AddTodo
-  },
+  }, 
   data() {
     return{
       todos: []
@@ -29,7 +30,10 @@ export default {
     }
   },
   created(){
-    
+    axios.get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+    // axios.get() returns a promise 
+      .then(res => this.todos = res.data) //this.todos refers to the empty array in data
+      .catch(err => console.log(err))
   }
 }
 </script>
